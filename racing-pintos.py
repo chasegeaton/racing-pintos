9cfc069dc7c9e8a3a8f545909153b8352d0d358e
Chase Eaton <chasegeaton@gmail.com>
2017-04-18 13:04:10 -0500

    Changed test dir name and added arg defaults

--- racing-pintos.py
+++ racing-pintos.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 
 # MIT License
-# 
+#
 # Copyright (c) 2017 Chase G. Eaton
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,7 +22,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# TODO: switch over to shutil for directory removal, creation, and copying
+# TODO: maybe switch over to shutil for directory removal, creation, and copying
 
 
 import os
@@ -40,7 +40,7 @@ loglev = logging.ERROR
 num_processes = 1
 num_tests = 1
 
-test_dir = 'race-testing'
+test_dir = 'testing-races'
 pintos_dir = 'src'
 project = 'threads'
 
@@ -170,10 +170,10 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('path', help='path to your pintos directory',
-                        metavar="PATH")
+                        default='src', metavar="PATH")
     parser.add_argument('project', help='name of the project you are testing',
                         choices=['threads', 'userprog', 'vm', 'filesys'],
-                        metavar='PROJECT')
+                        default='threads', metavar='PROJECT')
     parser.add_argument('-p', '--processes',
                         help='number of separate process to run tests in',
                         type=int, default=1, metavar='N')
